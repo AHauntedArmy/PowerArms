deef87c368863433def43d5efc849c6cf924d3ff
AHauntedArmy <extreme15@hotmail.co.uk>
2022-01-07 22:55:39 +0000

    added archiving script for monkemodmanager compatibility

---
+++ MMM archive.ps1
@@ -0,0 +1,8 @@
+# Needs to be at least that version, or mmm can't read the archive
+#Requires -Modules @{ ModuleName="Microsoft.PowerShell.Archive"; ModuleVersion="1.2.3" }
+$Name = "PowerArms" # Replace with your mods name
+$Version = "v1.0.0"
+mkdir BepInEx\plugins\$Name
+cp $Name\bin\Release\netstandard2.0\$Name.dll BepInEx\plugins\$Name\
+Compress-Archive -force .\BepInEx\ $Name-$Version.zip
+rmdir .\BepInEx\ -Recurse
